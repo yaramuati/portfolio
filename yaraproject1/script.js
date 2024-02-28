55449cc5bbc1ddc4c369e272ed437d345721c112
yaramuati <149670822+yaramuati@users.noreply.github.com>
2024-02-28 14:32:27 +0200

    Add files via upload

---
+++ yaraproject1/script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
